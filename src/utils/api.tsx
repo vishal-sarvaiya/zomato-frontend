114b513bde2fa05d91d9064401c4062f6f00f587
Vishal Sarvaiya <vishal.sarvaiya@technostacks.com>
2023-10-05 15:42:13 +0530

    vercel base_url changed

--- src/utils/api.tsx
+++ src/utils/api.tsx
@@ -1,4 +1,5 @@
-export const BASE_URL = "http://localhost:8000/"
+// export const BASE_URL = "http://localhost:8000/"
+export const BASE_URL = "https://zomato-backend-six.vercel.app"
 
 export const apiList = {
     register: "register",
